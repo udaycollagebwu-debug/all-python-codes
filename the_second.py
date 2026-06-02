9ca65d92792b12c714b007f72cba21857418901c
Uday sankar <udaycollagebwu@gmail.com>
2026-06-02 23:16:29 +0530

    the full code

---
+++ the_second.py
@@ -0,0 +1,5 @@
+# hear i am going to talk about the variables with details
+b="uday" # the b is hear a variable which is caring the name 
+a=19 # hear the a is caring the 19 value
+print(b)
+print(a)
