13a2fb0cd1d582052edcbce350cd257dc2855433
Uday sankar <udaycollagebwu@gmail.com>
2026-06-02 23:24:31 +0530

    THe basic code

---
+++ list.py
@@ -0,0 +1,3 @@
+# list is like a list of a numbers and enathing else ,the list is like so many number is one variyable
+list1=[1,2,3,4,'A','b','uday',9.89]
+print("The list that you give in the top of the program :",list1)
