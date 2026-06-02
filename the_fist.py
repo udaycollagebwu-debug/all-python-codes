8570125e03bd30f8836c7c96e2b659b65e1057bd
Uday sankar <udaycollagebwu@gmail.com>
2026-06-02 23:06:03 +0530

    the basic code has bin don

---
+++ the_fist.py
@@ -0,0 +1,6 @@
+#hear i will do a basic code witha a simpale and easy way.
+# take input a name age _- thay are the variables that i can say
+name=input("Enter your name :")
+age=input("Enter your or others age :")
+print("The name that you enterd is ",name)
+print("The age that you enterd Yours or Others age :",age)
