f89a0e711e438d33c74d562ab144fcfb27fffd06
Uday sankar <udaycollagebwu@gmail.com>
2026-06-10 08:21:43 +0530

    The code and the logic is don

---
+++ reverse.py
@@ -0,0 +1,11 @@
+#reverse number (print a number in reverse order)
+number =int(input("Enter a number : "))
+revers=0
+original=number
+while number!=0:
+    remincder=number%10
+    revers=revers*10+remincder
+    number=number//10
+print("The reverse number is :",revers)
+if(revers==original):
+    print("The number is a palindrome number !")
