23a88a3f8fc2b96b7394fd0d97dee7173daa0028
Uday sankar <udaycollagebwu@gmail.com>
2026-06-17 07:51:57 +0530

    code is don

--- function_prime.py
+++ function_prime.py
@@ -1,4 +1,13 @@
-def primeNUmber(n):
-    if n>2:
+def primeNumber(n):
+    if n < 2:
         return False
-    
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+num=int(input("Enter the nuber that you want to check the prime number :"))
+primeNumber(num)
+if(primeNumber(num)== False):
+    print("The number is not a Prime Number .")
+else:
+    print("The number is a prime number .")
