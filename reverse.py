f6c5587aa1caee4747d2ed9fc043d14dc50687da
Uday sankar <udaycollagebwu@gmail.com>
2026-06-24 09:31:36 +0530

    the code is upploded in upper case

--- reverse.py
+++ reverse.py
@@ -5,7 +5,7 @@ original=number
 while number!=0:
     remincder=number%10
     revers=revers*10+remincder
-    number=number//10
+    number=number//10 #this will gives us the output of integer .
 print("The reverse number is :",revers)
 if(revers==original):
     print("The number is a palindrome number !")
---
+++ string_upper.py
@@ -0,0 +1,5 @@
+#make all charactor in upper case 
+txt= "my name is uday sankar singha , i ma form ganjann uttar dinajpur "
+txt_upper=txt.upper()
+print(txt)
+print(txt_upper)
