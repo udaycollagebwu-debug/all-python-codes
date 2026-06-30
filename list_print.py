a4dc353dc3e65e1eadf21861058e1c473b4c85f6
Uday sankar <udaycollagebwu@gmail.com>
2026-06-30 09:20:47 +0530

    add a new code

---
+++ list_print.py
@@ -0,0 +1,9 @@
+name_list=['uday','raja','nabayan','ashok','radhika','balika','riya','nandani','daymonti','bidhut']
+print("The name list is :",name_list)
+print("print the name using the for loop :")
+for x in name_list:
+    print(x)
+print("The name has any 'y':")
+for x in name_list:
+    if 'y' in x:
+        print(x)
