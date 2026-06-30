476ea01668e1e4824bf6f4b7a59a9f8c3ea03109
Uday sankar <udaycollagebwu@gmail.com>
2026-06-30 09:26:46 +0530

    add somthins shorting codes

--- list_print.py
+++ list_print.py
@@ -7,3 +7,9 @@ print("The name has any 'y':")
 for x in name_list:
     if 'y' in x:
         print(x)
+print("The list heve to short that why use the sort() function .")
+name_list.sort()
+print("The short list is :",name_list)
+print ("The list in the revers order .")
+name_list.sort(reverse=True)
+print("The revers order list is :",name_list)
