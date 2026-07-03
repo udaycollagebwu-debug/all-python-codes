1fbcbe19f82f69eecfe09df79e6189e59716c492
Uday sankar <udaycollagebwu@gmail.com>
2026-07-03 22:07:52 +0530

    some change

---
+++ tuple1.py
@@ -0,0 +1,7 @@
+#in this we tesst the how to creat and add iteam in set 
+myset={"uday",19,"ganjana"}#set is a unorderd culoction o fiteams 
+print(myset)
+print(type(myset))
+myset.add("sankar")
+print(myset)
+ 
