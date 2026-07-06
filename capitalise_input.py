66b56970eac05cc4d22492aa1da6277c14a9b92d
Uday sankar <udaycollagebwu@gmail.com>
2026-07-06 09:22:20 +0530

    write a capitalise and uppercase codes

---
+++ capitalise_input.py
@@ -0,0 +1,3 @@
+input_string=input("Enter a string :")
+print("The input string is :",input_string)
+print("The capitalise string is : ",input_string.capitalize()) #hear onl the first latter of the line will be capitaliz 
---
+++ uppercase_input.py
@@ -0,0 +1,3 @@
+input_sstring=input("Enter the string :")
+print("The input string is :",input_sstring)
+print("The uppercase string is : ",input_sstring.upper())# in this we get all the alphabet in the upper case latter
