092df8b0114a77c0132f964d9df71e2d4c1c8db0
Uday sankar <udaycollagebwu@gmail.com>
2026-07-14 10:52:46 +0530

    the first code of dictonary

---
+++ dictionary1.py
@@ -0,0 +1,11 @@
+#using dictionary function
+details={
+    "name":"uday sankar singha",
+    "collage" : "brainwear university",
+    "collage_id":"BWU/BTW/25/418",
+    "age":19
+}
+print(details)
+print(type(details))
+
+print("Name :",details["name"])
