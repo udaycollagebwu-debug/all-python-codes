b3d4269f8f86ebf74dc6206524516da305ed5b6e
Uday sankar <udaycollagebwu@gmail.com>
2026-08-19 10:17:50 +0530

    basic

---
+++ set/looping_set.py
@@ -0,0 +1,5 @@
+s ={12,13,14,15,16,17,18,22,51,67,80}
+for item in s:
+    print(item)
+    # print(type(item))  this print the int type 
+print(type(s)) # print the set type 
---
+++ set/set1.py
@@ -0,0 +1,18 @@
+set1 = {} #cant decclear the ste with this {}
+
+print(type(set1))
+set2 = {'name','class'}  #this is a set
+
+print(type(set2))
+
+set3 =set()
+print(type(set3))
+
+# set4 = {'name','you',6,7,8}
+# print(set4[3])                 cant use the index to axces the element.
+
+#set ={[2,3,4,5,0],[2,5,7,9,12,4]}  cant stor list inside a set.
+
+set5 = {(2,4,7,8,8),(1,2,3,4)}
+
+print(set5)
