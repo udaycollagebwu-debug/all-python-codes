ae5c83429d74c6cdf51dc1975f2de3fbf4cf4101
Uday sankar <udaycollagebwu@gmail.com>
2026-08-21 20:45:43 +0530

    basic

---
+++ modules/car_check_fine.py
@@ -0,0 +1,9 @@
+def check_speed(sp):
+    if sp >100:
+        print(f"The speed is to high so you have to pay so much {100} rupy")
+    elif sp > 90:
+        print(f"The speed is too high pay {700} rupy")
+    elif sp > 70:
+        print(f"The speed is too high pay {500} rupy")
+    else:
+        print("Your good you dont have to pay anything to the pulic ")
---
+++ modules/car_speed.py
@@ -0,0 +1,3 @@
+import car_check_fine
+
+car_check_fine.check_speed(90)
