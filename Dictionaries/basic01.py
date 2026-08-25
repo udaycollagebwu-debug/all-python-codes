7bedd9e7d4546202d4bef1442bcc68d8d100a360
Uday sankar <udaycollagebwu@gmail.com>
2026-08-25 09:42:24 +0530

    basic sintax o fdictionaries

---
+++ Dictionaries/basic01.py
@@ -0,0 +1,7 @@
+# change the data type to dictionarie
+
+dicto_1 = dict(name = "uday sankar singha",course = "python for machin learning ",
+               age = 19,Roll = 418)
+
+print(type(dicto_1))
+print(dicto_1)
---
+++ Dictionaries/basic_intro.py
@@ -0,0 +1,10 @@
+# What is dictionary ?
+# Dictionary is a build in data type in python , this datatype store data in key-value pears .
+# example -
+Student_details ={
+    'Name':"Uday sankar singha",
+    'Roll':418,
+    "cours":'Python for machin learning'
+}
+print(Student_details)
+print("The data type of the student_details is :",type(Student_details))
