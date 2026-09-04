fc3b2b3344569717726ad9a918ec9e4c230b487d
Uday sankar <udaycollagebwu@gmail.com>
2026-09-04 12:08:44 +0530

    nwe file

---
+++ Class_OPP/spectial operation/operations_more1.py
@@ -0,0 +1,19 @@
+# ti check the number is bigger then 'a'?
+# if a < b
+# check the " < " and " > "
+
+
+# check the " < "
+a = 5
+b = 4
+if a.__lt__(b):
+    print(b," is biger then the the ",a)
+else:
+    print(a," is biger then the the ",b)
+    
+# check the " > "
+
+if a.__gt__(b):
+    print(a," is biger then the the ",b)
+else:
+    print(b," is biger then the the ",a)
