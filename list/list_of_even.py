5496fb2c8fcc75c52624614595918d7faa2095fd
Uday sankar <udaycollagebwu@gmail.com>
2026-09-10 09:14:04 +0530

    a simpale codes

---
+++ list/list_of_even.py
@@ -0,0 +1,11 @@
+# import random
+
+
+# even_numbers = [random.randint(10, 30) for _ in range(10) if random.randint(10, 30) % 2 == 0]
+
+# print("The even number list is:", even_numbers)
+
+
+even_number = list(range(10,30,2))
+
+print(even_number)
